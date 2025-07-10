d7ba0ad866b8addf1478b74709306dceca5667b3
Vitalii Savitskii <visa@ecco.com>
2025-07-10 15:36:00 +0200

    Add backends definition and http errors conversion

--- src/services/backends.rs
+++ src/services/backends.rs
@@ -1,1 +1,23 @@
+use std::sync::Arc;
+use async_trait::async_trait;
+use crate::services::base::types::SchemaRepository;
+
 pub mod kubernetes;
+
+#[allow(dead_code)]
+pub trait Backend: SchemaRepositorySource + Send + Sync {
+    
+}
+
+pub trait SchemaRepositorySource: Send + Sync {
+    #[allow(dead_code)]
+    fn get_schemas_repository(&self) -> Arc<SchemaRepository>;
+}
+
+#[async_trait]
+#[allow(dead_code)]
+pub trait BackendConfiguration: Send + Sync + Sized {
+    type BackendSettings;
+    
+    async fn configure(mut self, cm: &Self::BackendSettings, instance_name: String) -> anyhow::Result<Arc<dyn Backend>>;
+}
