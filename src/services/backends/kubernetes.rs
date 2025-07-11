80443c17e04948c226f11fee7384c71d202bef41
Vitalii Savitskii <visa@ecco.com>
2025-07-11 14:42:13 +0200

    Add kubernetes config loader (#5)
    * Add the Kubernetes configuration loader
    
    * Add the Kubernetes configuration loader
    
    * Make kubeconfig_loader public
    
    * Make kubeconfig_loader send + sync
    
    * Make kubeconfig_loader send + sync
    
    * Fix formatting issues

--- src/services/backends/kubernetes.rs
+++ src/services/backends/kubernetes.rs
@@ -1,5 +1,6 @@
 pub mod kubernetes_resource_manager;
 pub mod repositories;
-
 #[cfg(test)]
 mod tests;
+
+pub mod kubeconfig_loader;
---
+++ src/services/backends/kubernetes/kubeconfig_loader.rs
@@ -0,0 +1,59 @@
+use anyhow::bail;
+use async_trait::async_trait;
+use kube::Config;
+use kube::config::Kubeconfig;
+use log::{debug, info};
+use serde_yml::from_str;
+use std::process::Command;
+use std::sync::Arc;
+
+pub fn from_command() -> Arc<dyn KubeConfigLoader<ConfigSource = String>> {
+    Arc::new(ExecutableKubeConfigLoader)
+}
+
+pub fn from_file() -> Arc<dyn KubeConfigLoader<ConfigSource = String>> {
+    Arc::new(FileKubeConfigLoader)
+}
+
+#[async_trait]
+pub trait KubeConfigLoader {
+    type ConfigSource;
+    async fn load(&self, source: &Self::ConfigSource) -> anyhow::Result<Config>;
+}
+
+struct ExecutableKubeConfigLoader;
+
+#[async_trait]
+impl KubeConfigLoader for ExecutableKubeConfigLoader {
+    type ConfigSource = String;
+
+    async fn load(&self, source: &Self::ConfigSource) -> anyhow::Result<Config> {
+        info!("Configuring Kubernetes backend with command: {:?}", source);
+        let output = Command::new("sh").arg("-c").arg(source).output()?;
+        if !output.status.success() {
+            bail!(
+                "Failed to execute command: {:?}",
+                String::from_utf8_lossy(&output.stderr)
+            );
+        }
+        let kubeconfig_string = String::from_utf8(output.stdout)?;
+        debug!("Kubeconfig used by the backend:\n{:?}", kubeconfig_string);
+        let kubeconfig: Kubeconfig = from_str(&kubeconfig_string)?;
+        Ok(Config::from_custom_kubeconfig(kubeconfig, &Default::default()).await?)
+    }
+}
+
+struct FileKubeConfigLoader;
+
+#[async_trait]
+impl KubeConfigLoader for FileKubeConfigLoader {
+    type ConfigSource = String;
+
+    async fn load(&self, source: &Self::ConfigSource) -> anyhow::Result<Config> {
+        info!("Configuring Kubernetes backend with kubeconfig file: {:?}", source);
+        let kubeconfig_string = std::fs::read_to_string(source)?;
+        debug!("Kubeconfig used by the backend:\n{:?}", kubeconfig_string);
+        let kubeconfig: Kubeconfig = from_str(&kubeconfig_string)?;
+        Ok(Config::from_custom_kubeconfig(kubeconfig, &Default::default()).await?)
+    }
+}
