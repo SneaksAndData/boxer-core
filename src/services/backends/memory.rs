0d958284956c9baf610975003966c0043462eefc
Vitalii Savitskii <visa@ecco.com>
2025-07-15 10:51:24 +0200

    Split the UpsertRepository interface
    Part of #41
    
    Since we use an immutable trie implementation for mapping incoming requests, I don't want to implement the `delete` method.
    
    Additionally, the insertions and readings are handled by different subsystems, and I need two different interfaces for them.

--- src/services/backends/memory.rs
+++ src/services/backends/memory.rs
@@ -1,10 +1,27 @@
-use crate::services::base::upsert_repository::UpsertRepository;
+use crate::services::base::upsert_repository::{CanDelete, ReadOnlyRepository, UpsertRepository};
 use anyhow::bail;
 use async_trait::async_trait;
 use std::collections::HashMap;
 use std::fmt::Debug;
 use std::hash::Hash;
 use tokio::sync::RwLock;
+
+#[async_trait]
+impl<Entity, Key> ReadOnlyRepository<Key, Entity> for RwLock<HashMap<Key, Entity>>
+where
+    Entity: Clone + Send + Sync,
+    Key: Debug + Eq + Hash + Send + Sync,
+{
+    type ReadError = anyhow::Error;
+
+    async fn get(&self, key: Key) -> Result<Entity, Self::ReadError> {
+        let read_guard = self.read().await;
+        match (*read_guard).get(&key) {
+            Some(entity) => Ok(entity.clone()),
+            None => bail!("Entity {:?} not found", key),
+        }
+    }
+}
 
 #[async_trait]
 impl<Entity, Key> UpsertRepository<Key, Entity> for RwLock<HashMap<Key, Entity>>
@@ -14,28 +31,29 @@ where
 {
     type Error = anyhow::Error;
 
-    async fn get(&self, key: Key) -> Result<Entity, Self::Error> {
-        let read_guard = self.read().await;
-        match (*read_guard).get(&key) {
-            Some(entity) => Ok(entity.clone()),
-            None => bail!("Entity {:?} not found", key),
-        }
-    }
-
     async fn upsert(&self, key: Key, entity: Entity) -> Result<(), Self::Error> {
         let mut write_guard = self.write().await;
         (*write_guard).insert(key, entity);
         Ok(())
     }
 
-    async fn delete(&self, key: Key) -> Result<(), Self::Error> {
-        let mut write_guard = self.write().await;
-        (*write_guard).remove(&key);
-        Ok(())
-    }
-
     async fn exists(&self, key: Key) -> Result<bool, Self::Error> {
         let read_guard = self.read().await;
         Ok((*read_guard).get(&key).is_some())
+    }
+}
+
+#[async_trait]
+impl<Key, Entity> CanDelete<Key, Entity> for RwLock<HashMap<Key, Entity>>
+where
+    Entity: Send + Sync + Clone,
+    Key: Send + Sync + Eq + Hash + Debug,
+{
+    type DeleteError = anyhow::Error;
+
+    async fn delete(&self, key: Key) -> Result<(), Self::DeleteError> {
+        let mut write_guard = self.write().await;
+        (*write_guard).remove(&key);
+        Ok(())
     }
 }
