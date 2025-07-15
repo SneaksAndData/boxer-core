d768909b2a331cd3694059a7599b7cff05b718b2
Vitalii Savitskii <visa@ecco.com>
2025-07-15 10:52:35 +0200

    Fix the diff

--- src/services/base/upsert_repository.rs
+++ src/services/base/upsert_repository.rs
@@ -2,6 +2,18 @@ use async_trait::async_trait;
 
 #[async_trait]
 /// Represents a repository for policies
+pub trait UpsertRepository<Key, Entity>: ReadOnlyRepository<Key, Entity> + Send + Sync {
+    type Error;
+
+    /// Updates or inserts a policy by id
+    async fn upsert(&self, key: Key, entity: Entity) -> Result<(), Self::Error>;
+
+    /// Checks if an object exists
+    async fn exists(&self, key: Key) -> Result<bool, Self::Error>;
+}
+
+#[async_trait]
+/// Represents a repository for policies
 pub trait ReadOnlyRepository<Key, Entity>: Send + Sync {
     type ReadError;
 
@@ -18,17 +30,6 @@ pub trait CanDelete<Key, Entity>: Send + Sync {
     async fn delete(&self, key: Key) -> Result<(), Self::DeleteError>;
 }
 
-#[async_trait]
-/// Represents a repository for policies
-pub trait UpsertRepository<Key, Entity>: ReadOnlyRepository<Key, Entity> + Send + Sync {
-    type Error;
-
-    /// Updates or inserts a policy by id
-    async fn upsert(&self, key: Key, entity: Entity) -> Result<(), Self::Error>;
-
-    /// Checks if an object exists
-    async fn exists(&self, key: Key) -> Result<bool, Self::Error>;
-}
 
 pub trait UpsertRepositoryWithDelete<Key, Entity>: UpsertRepository<Key, Entity> + CanDelete<Key, Entity> {
     // This trait is a marker trait that combines UpsertRepository and CanDelete
