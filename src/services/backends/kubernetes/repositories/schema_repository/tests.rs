d7329b7b5d1fc2424fb8b4f1bfcb82fdc0a2d350
Vitalii Savitskii <visa@ecco.com>
2025-08-06 12:38:05 +0200

    Fix formatting issues

--- src/services/backends/kubernetes/repositories/schema_repository/tests.rs
+++ src/services/backends/kubernetes/repositories/schema_repository/tests.rs
@@ -4,7 +4,7 @@ use crate::services::base::upsert_repository::UpsertRepository;
 use crate::testing::{create_namespace, get_kubeconfig};
 use std::sync::Arc;
 use std::time::Duration;
-use test_context::{test_context, AsyncTestContext};
+use test_context::{AsyncTestContext, test_context};
 
 const DEFAULT_TEST_TIMEOUT: Duration = Duration::from_secs(10);
 
