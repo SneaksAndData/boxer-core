accb293099fb3c0384b8de5eae66d9ada8c8fdea
Vitalii Savitskii <visa@ecco.com>
2025-08-11 13:31:04 +0200

    Add the K8S -> HTTP status code conversion (#24)

---
+++ src/http/conversions.rs
@@ -0,0 +1,14 @@
+use crate::services::backends::kubernetes::kubernetes_resource_manager::status::Status;
+
+impl From<Status> for actix_web::Error {
+    fn from(err: Status) -> Self {
+        match err {
+            Status::Conflict => actix_web::error::ErrorConflict(err.to_string()),
+            Status::NotFound(_) => actix_web::error::ErrorNotFound(err.to_string()),
+            Status::Deleted(_) => actix_web::error::ErrorGone(err.to_string()),
+            Status::Timeout(message) => actix_web::error::ErrorRequestTimeout(message),
+            Status::ConversionError(cause) => actix_web::error::ErrorInternalServerError(cause),
+            Status::Other(e) => actix_web::error::ErrorInternalServerError(e),
+        }
+    }
+}
