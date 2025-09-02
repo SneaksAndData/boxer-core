9231aa89d79f328c43e038c6726c0338925e13be
Vitalii Savitskii <visa@ecco.com>
2025-09-02 14:23:15 +0200

    Add deserialize crate

--- src/services/observability/open_telemetry/settings.rs
+++ src/services/observability/open_telemetry/settings.rs
@@ -1,7 +1,9 @@
 use crate::services::observability::open_telemetry::logging::settings::LogSettings;
 use crate::services::observability::open_telemetry::metrics::settings::MetricsSettings;
 use crate::services::observability::open_telemetry::tracing::settings::TracingSettings;
+use serde::Deserialize;
 
+#[derive(Debug, Deserialize)]
 pub struct OpenTelemetrySettings {
     pub log_settings: LogSettings,
     pub metrics_settings: MetricsSettings,
