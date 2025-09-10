68f06aa9867b91a5824f89d4bf416cb76b22065a
Vitalii Savitskii <visa@ecco.com>
2025-09-10 16:09:10 +0200

    Add environment name to logs

--- src/services/observability/open_telemetry/logging.rs
+++ src/services/observability/open_telemetry/logging.rs
@@ -2,12 +2,20 @@ pub mod settings;
 
 use anyhow::Result;
 use log::Log;
+use opentelemetry::KeyValue;
 use opentelemetry_otlp::LogExporter;
 use opentelemetry_sdk::logs::SdkLoggerProvider;
+use opentelemetry_sdk::Resource;
 
-pub fn init_logger() -> Result<Box<dyn Log>> {
+pub fn init_logger(environment: String) -> Result<Box<dyn Log>> {
     let exporter = LogExporter::builder().with_tonic().build()?;
-    let provider = SdkLoggerProvider::builder().with_batch_exporter(exporter).build();
+    let environment = Resource::builder()
+        .with_attribute(KeyValue::new("Environment", environment))
+        .build();
+    let provider = SdkLoggerProvider::builder()
+        .with_resource(environment)
+        .with_batch_exporter(exporter)
+        .build();
 
     let appender = opentelemetry_appender_log::OpenTelemetryLogBridge::new(&provider);
     let logger = Box::new(appender);
