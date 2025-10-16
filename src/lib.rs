dd34f93baaf314290cbc4c7858783e521d82582d
Vitalii Savitskii <visa@ecco.com>
2025-10-16 12:31:32 +0200

    Fix

--- src/lib.rs
+++ src/lib.rs
@@ -5,4 +5,6 @@ pub mod contracts;
 pub mod http;
 pub mod services;
 // #[cfg(feature = "testing")]
+// COVERAGE: disabled since the module is only compiled for testing purposes
+#[cfg_attr(coverage, coverage(off))]
 pub mod testing;
