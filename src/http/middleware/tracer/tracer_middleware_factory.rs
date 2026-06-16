a60005faa067f3902495655b70a1ede021a6596c
Vitalii Savitskii <visa@ecco.com>
2026-06-16 12:38:21 +0200

    Implement tracer middleware (#113)
    * Implement tracer middleware
    
    Part of #71
    Also related to #70
    
    * Build fix
    
    * Fix formatting issues

---
+++ src/http/middleware/tracer/tracer_middleware_factory.rs
@@ -0,0 +1,42 @@
+use super::TraceMiddleware;
+use actix_web::Error;
+use actix_web::dev::{Service, ServiceRequest, ServiceResponse, Transform};
+use futures_util::future::LocalBoxFuture;
+use std::sync::Arc;
+
+/// Factory for the tracer middleware
+pub struct TracerMiddlewareFactory {
+    span_name: &'static str,
+}
+
+impl TracerMiddlewareFactory {
+    /// The Tracer middleware constructor
+    /// parameter `span_name` - Name of the span for the tracer
+    pub fn new(span_name: &'static str) -> Self {
+        TracerMiddlewareFactory { span_name }
+    }
+}
+
+impl<Next, Body> Transform<Next, ServiceRequest> for TracerMiddlewareFactory
+where
+    Next: Service<ServiceRequest, Response = ServiceResponse<Body>, Error = Error> + 'static,
+    Next::Future: 'static,
+    Body: 'static,
+{
+    type Response = ServiceResponse<Body>;
+    type Error = Error;
+    type Transform = TraceMiddleware<Next>;
+    type InitError = ();
+    type Future = LocalBoxFuture<'static, Result<TraceMiddleware<Next>, Self::InitError>>;
+
+    fn new_transform(&self, next: Next) -> Self::Future {
+        let span_name = self.span_name;
+        Box::pin(async move {
+            let mw = TraceMiddleware {
+                next: Arc::new(next),
+                span_name,
+            };
+            Ok(mw)
+        })
+    }
+}
