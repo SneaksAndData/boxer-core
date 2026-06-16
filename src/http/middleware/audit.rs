97835ace8b69d26d89bff706638dbef24d595d22
Vitalii Savitskii <visa@ecco.com>
2026-06-16 14:13:07 +0200

    Move services from boxer-issuer

--- src/http/middleware/audit.rs
+++ src/http/middleware/audit.rs
@@ -1,4 +1,14 @@
+use actix_web::dev::HttpServiceFactory;
+use actix_web::web;
+
 pub mod audit_recorder;
 pub mod audited_error;
 pub mod audited_request;
 pub mod begin_audit_chain;
+pub mod external_token;
+
+pub fn audit_middleware<S: HttpServiceFactory + 'static>(target: S) -> impl HttpServiceFactory {
+    web::scope("")
+        .wrap_fn(external_token::<AuditedRequest, actix_web::Error>)
+        .service(target)
+}
