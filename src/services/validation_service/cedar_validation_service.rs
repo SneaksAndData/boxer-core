f0c5dd6b224035dbf159718dc00ea6ee52769403
Vitalii Savitskii <visa@ecco.com>
2026-07-06 09:04:21 +0200

    fix formatting issues

--- src/services/validation_service/cedar_validation_service.rs
+++ src/services/validation_service/cedar_validation_service.rs
@@ -1,5 +1,5 @@
-use crate::services::audit::events::authorization_audit_event::AuthorizationAuditEvent;
 use crate::services::audit::AuditService;
+use crate::services::audit::events::authorization_audit_event::AuthorizationAuditEvent;
 use crate::services::base::upsert_repository::ReadOnlyRepository;
 use crate::services::observability::open_telemetry::metrics::authorization_metric::AuthorizationMetric;
 use crate::services::observability::open_telemetry::metrics::metric_recorders::token_accepted::TokenAccepted;
@@ -7,12 +7,12 @@ use crate::services::observability::open_telemetry::metrics::metric_recorders::t
 use crate::services::observability::open_telemetry::metrics::provider::MetricsProvider;
 use crate::services::observability::open_telemetry::tracing::start_trace;
 use crate::services::service_provider::ServiceProvider;
+use crate::services::validation_service::ValidationService;
 use crate::services::validation_service::path_segment::PathSegment;
 use crate::services::validation_service::request_context::RequestContext;
 use crate::services::validation_service::request_segment::RequestSegment;
 use crate::services::validation_service::required_claims::RequiredClaims;
 use crate::services::validation_service::schema_provider::SchemaProvider;
-use crate::services::validation_service::ValidationService;
 use async_trait::async_trait;
 use cedar_policy::{Authorizer, Context, Entities, EntityUid, PolicySet, Request};
 use log::{debug, info};
