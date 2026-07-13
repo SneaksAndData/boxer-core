e6c400bd7f0322c55a94f29402ec7d382155f692
Vitalii Savitskii <visa@ecco.com>
2026-07-13 13:26:44 +0200

    Fix unit test

--- src/http/middleware/audit/tests.rs
+++ src/http/middleware/audit/tests.rs
@@ -9,23 +9,23 @@ use crate::services::audit::chained::token_audit_event::TokenAuditEvent;
 use crate::services::audit::events::token_validation_event::TokenValidationResult;
 use crate::services::base::upsert_repository::ReadOnlyRepository;
 use crate::services::observability::open_telemetry::metrics::provider::MetricsProvider;
-use crate::services::token_decryption_service::TokenDecryptionService;
 use crate::services::token_decryption_service::encryption_keys::EncryptionKeys;
 use crate::services::token_decryption_service::token_settings::TokenValidationSettings;
-use crate::services::token_provider::TokenProvider;
+use crate::services::token_decryption_service::TokenDecryptionService;
 use crate::services::token_provider::encrypted_token_service::EncryptedTokenService;
 use crate::services::token_provider::external_identity::ExternalIdentity;
 use crate::services::token_provider::principal::Principal;
 use crate::services::token_provider::principal_service::PrincipalService;
-use crate::services::validation_service::ValidationService;
+use crate::services::token_provider::TokenProvider;
 use crate::services::validation_service::cedar_validation_service::CedarValidationService;
 use crate::services::validation_service::path_segment::PathSegment;
 use crate::services::validation_service::request_context::RequestContext;
 use crate::services::validation_service::request_segment::RequestSegment;
 use crate::services::validation_service::schema_provider::SchemaProvider;
+use crate::services::validation_service::ValidationService;
 use actix_web::http::StatusCode;
-use actix_web::web::{ReqData, scope};
-use actix_web::{App, HttpMessage, HttpRequest, HttpResponse, test, web};
+use actix_web::web::{scope, ReqData};
+use actix_web::{test, web, App, HttpMessage, HttpRequest, HttpResponse};
 use anyhow::Result;
 use assert_matches::assert_matches;
 use async_trait::async_trait;
@@ -284,7 +284,7 @@ async fn test_token_v1() {
     let webapp = App::new().service(pipeline);
     let request = test::TestRequest::get()
         .uri("/token")
-        .append_header(("Authorization", token))
+        .append_header(("Authorization", format!("Bearer {}", token)))
         .append_header(("X-Original-URL", "http://example.com"))
         .append_header(("X-Original-Method", "POST"))
         .to_request();
