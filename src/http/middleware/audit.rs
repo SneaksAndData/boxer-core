136068026e6e9f0555d38dbafe271d1eb34caa49
Vitalii Savitskii <visa@ecco.com>
2026-09-21 15:38:30 +0200

    Add one more fix

--- src/http/middleware/audit.rs
+++ src/http/middleware/audit.rs
@@ -56,6 +56,6 @@ pub async fn finalize_on_fail(
             Ok(ServiceResponse::new(req, restored))
         }
         Ok(response) => Ok(response.map_into_boxed_body()),
-        Err(error) => Err(error)
+        Err(error) => Err(error),
     }
 }
